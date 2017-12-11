7cc9c634bf2ce42943226867c7faaa19bce1b8ee
dyzio18 <dyzio18@gmail.com>
2017-12-11 19:36:29 +0100

    start working with passport.js

---
+++ config/keys.example.js
@@ -0,0 +1,12 @@
+/**
+ * Get your own credentials from google API
+ */
+
+/*
+
+const googleClientID = 'CLIENT ID';
+const googleClientSecret = 'SECRET';
+
+export {googleClientID,googleSecret};
+
+*/
