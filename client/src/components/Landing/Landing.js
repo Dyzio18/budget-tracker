ee52c91e5b9ca2530bb754639c45bfc29abea63c
dyzio18 <dyzio18@gmail.com>
2018-08-04 12:22:33 +0200

    update 2018-08-04

--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
-import SectionExplanation from './SectionExplanation';
+import SectionInfo from './SectionExplanation';
 
 class Landing extends Component {
 
   render() {
     return (
-        <div className="landing">
-            <div className="jumbotron jumbotron-fluid">
-                <div class="container">
-                    <h1 className="display-4">Habits Tracker</h1>
-                    <p className="lead">Aplikacja do śledzenia nawyków. Monitoruj swoje rutyny i codzienne czynności.</p>
-                    <p className="lead">Dzięki systemowi grywalizacji nauka nawyków stanie się zabawą i świetnym wyzwaniem. 
-                        Zdobywaj kolejne poziomy i odznaki a nawyki same wejdą Ci w krew!</p>
-                    <a href="/auth/google" className="btn btn-primary">
-                        <i class="fab fa-google"> </i> Sign in with Google
-                    </a>
+        <div>
+            <header className="landing__header">
+                <h1>Habits Tracker</h1>
+                <a href="/auth/google" className="btn btn__googleLogin">
+                    <i className="fab fa-google"> </i> Sign in with Google
+                </a>
+            </header>
+            <section className="landing__info">
+                <div className="container">
+                    <SectionInfo/>
                 </div>
-            </div> 
-            
-            <SectionExplanation/>
+            </section>
         </div>
     )
   }
--- client/src/components/Landing/SectionExplanation.js
+++ client/src/components/Landing/SectionExplanation.js
@@ -1,37 +1,42 @@
 import React, { PureComponent } from 'react';
 
-export default class SectionExplanation extends PureComponent {
+export default class SectionInfo extends PureComponent {
     render() {
         return (
-            <section className="row">
-                <div className="col-sm-12 col-md-4 col-lg-4">
-                    <div className="card text-white bg-primary">
-                        <div className="card-header">Nawyki</div>
-                        <div className="card-body">
-                            <h5 className="card-title">Codzienny rozwój</h5>
-                            <p className="card-text">Codziennie wykonujesz rutynowe czynności, drobna zmiana w swoich przyzwyczajeniach pozwoli Ci na zmiane na lepsze </p>
-                        </div>
+        <div>
+            <div className="info__box">
+                <div className="info__card">
+                    <div className="info__header info__header--one">
+                        <img src={require('./hands_plants.svg')} alt="sasadź nawyki"/>
+                    </div>
+                    <div className="info__desc">
+                        <h2 className="info__subtitle" >1. Posadzić nawyki</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur metus quis orci tincidunt consectetur. Fusce dignissim, massa sed porttitor dictum, leo metus imperdiet arcu.</p>
                     </div>
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-4">
-                    <div className="card text-white bg-secondary">
-                        <div className="card-header">Grywalizacja</div>
-                        <div className="card-body">
-                            <h5 className="card-title">System nagród i poziomów</h5>
-                            <p className="card-text">Niech umacnianie nawyków będzie zabawą dzięki systemowi grywalizacji. Zdobywaj kolejne poziomy i nagrody.</p>
-                        </div>
+
+                <div className="info__card">
+                    <div className="info__header info__header--two">
+                        <img src={require('./hands_plants2.svg')}  alt="rozwijaj nawyki"/>
+                    </div>
+                    <div className="info__desc">
+                        <h2 className="info__subtitle" >2. Rozwijaj</h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur metus quis orci tincidunt consectetur. Fusce dignissim, massa sed porttitor dictum, leo metus imperdiet arcu.</p>
                     </div>
                 </div>
-                <div className="col-sm-12 col-md-4 col-lg-4">
-                    <div className="card text-white bg-info">
-                        <div className="card-header">Self-tracking</div>
-                        <div className="card-body">
-                            <h5 className="card-title">Monitorowanie swoich postępów</h5>
-                            <p className="card-text">Za pomocą aplikacji możesz śledzić swoje postępy. Zobacz jak rosła siła Twojego nawyku w czasie na wykresie.</p>
-                        </div>
+
+                <div className="info__card">
+                    <div className="info__header info__header--three">
+                        <img src={require('./idea_plants.svg')} alt="pielegnuj nawyki" />
+                    </div>
+                    <div className="info__desc">
+                        <h2 className="info__subtitle" >3. Pielęgnuj </h2>
+                        <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla efficitur metus quis orci tincidunt consectetur. Fusce dignissim, massa sed porttitor dictum, leo metus imperdiet arcu.</p>
                     </div>
                 </div>
-            </section>
+
+            </div>
+        </div>
         )
   }
 };
